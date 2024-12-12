e2db6529d04601b0f600fe3c89ed04b10ab9f318
aryanx9 <armanhossainaful@gmail.com>
2024-12-11 22:35:13 -0800

    Update info.py

--- info.py
+++ info.py
@@ -112,9 +112,9 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 
 # Token Verification Info :
-VERIFY = bool(environ.get('VERIFY', False))
-VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', '')
-VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', '')
+VERIFY = bool(environ.get('VERIFY', True))
+VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'zxlink.in')
+VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', 'e39c9b0dd71ec6f3f36c4815a18bba24fab10bcf')
 VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', '')
 
 # If You Fill Second Shortner Then Bot Attach Both First And Second Shortner And Use It For Verify.
